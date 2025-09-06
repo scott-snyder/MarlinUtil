d356907819d75c137bdd258724a39a1c1cddc101
sss <sss@karma>
2025-09-06 19:32:55 -0400

    Refresh of warn

--- source/include/DDMarlinCED.h
+++ source/include/DDMarlinCED.h
@@ -295,7 +295,7 @@ protected:
 
   // FIXME: Not so elegant, refine! Use iterators, templates etc. See drawHitCollectionsByType(...).
   // helper method to draw hit collections by MC Contribution
-  static void drawHitCollectionsByMCContribution(LCEvent* event, MCParticle* MCP, int marker, int size, 
+  static void drawHitCollectionsByMCContribution(LCEvent* event, MCParticle* mcp, int marker, int size, 
              unsigned int color, unsigned int layer=0) {
     
     std::vector< std::string >::const_iterator iter;
@@ -313,11 +313,11 @@ protected:
 
         SimTrackerHit* hit = dynamic_cast<SimTrackerHit*>(col->getElementAt(i));
     
-        if (hit->getMCParticle() == MCP) {
+        if (hit->getMCParticle() == mcp) {
           double x = hit->getPosition()[0];
           double y = hit->getPosition()[1];
           double z = hit->getPosition()[2];
-            ced_hit_ID(x,y,z,marker, layer,size,color, MCP->id());
+            ced_hit_ID(x,y,z,marker, layer,size,color, mcp->id());
 
 
         }
@@ -336,7 +336,7 @@ protected:
 
         bool found = false;
         for (int j = 0; j < nMC; ++j) {
-          if (hit->getParticleCont(j) == MCP) {
+          if (hit->getParticleCont(j) == mcp) {
             found = true; 
             break;
           }
@@ -346,7 +346,7 @@ protected:
           double x = hit->getPosition()[0];
           double y = hit->getPosition()[1];
           double z = hit->getPosition()[2];
-            ced_hit_ID(x,y,z,marker,layer,size,color, MCP->id());
+            ced_hit_ID(x,y,z,marker,layer,size,color, mcp->id());
         }
       }
       }
